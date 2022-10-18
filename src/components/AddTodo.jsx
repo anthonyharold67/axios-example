d66eaa1c8a2d7b63a239307278df4b15cdc84184
AnthonyHarold67 <anthonyharold67@gmail.com>
2022-10-18 12:31:10 +0300

    axios finished

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Row } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+function AddTodo({postTodo}) {
+  const [inputVal,setInputVal] = useState("")  
+
+  const handleClick = ()=>{
+    if(inputVal){
+        const todo = {
+            "todo":inputVal,
+            "isDone": false
+        }
+        postTodo(todo)
+        setInputVal("")
+    }
+  }
+
+  return (
+    <Row>
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Add todo..."
+          aria-label="Add todo..."
+          aria-describedby="basic-addon2"
+          value={inputVal}
+          onChange={(e)=>setInputVal(e.target.value)}
+        />
+        <Button variant="outline-secondary" id="button-addon2" onClick={handleClick}>
+          Add Todo
+        </Button>
+      </InputGroup>
+    </Row>
+  );
+}
+
+export default AddTodo;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Container from 'react-bootstrap/Container'
+import AddTodo from '../components/AddTodo'
+import TodoList from '../components/TodoList'
+
+const Home = () => {
+    const [todos,setTodos] = useState()
+  
+    const url = "https://634e52d94af5fdff3a589e1a.mockapi.io/api/todos/"
+  //+ read işlemi için
+  const getTodos = async () => {
+    // const response = await axios.get(url)
+    // const {data} = response
+    // console.log(response.data)
+    // console.log(data)
+
+    const {data} = await axios.get(url)
+    console.log(data)
+    setTodos(data)
+  }
+ //& create işlemi için
+  const postTodo = async (item) => {
+      await axios.post(url,item)
+    //   setTodos([...todos,item])
+    getTodos()
+  }
+
+  //? delete işlemi için
+  const deleteTodo = async (id) =>{
+    await axios.delete(`${url}${id}`)
+    getTodos()
+  }
+  useEffect(()=>{
+    getTodos()
+  },[])
+
+  return (
+    <Container className="mt-4">
+        <AddTodo postTodo={postTodo} />
+        <TodoList todos={todos} deleteTodo={deleteTodo} />
+    </Container>
+  )
+}
+
+export default Home
